e939f0eac00c644d108914ac1090910b07806ce0
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-10-17 19:51:30 +0600

    optionals

--- client/src/main/java/tech/lapsa/insurance/dao/GeneralDAO.java
+++ client/src/main/java/tech/lapsa/insurance/dao/GeneralDAO.java
@@ -1,16 +1,36 @@
 package tech.lapsa.insurance.dao;
 
-import java.util.List;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import tech.lapsa.java.commons.function.MyObjects;
 
 public interface GeneralDAO<T, I> {
-    T findById(I id) throws EntityNotFound, PeristenceOperationFailed;
 
-    T findByIdByPassCache(I id) throws EntityNotFound, PeristenceOperationFailed;
+    default T findById(I id) throws EntityNotFound {
+	return optionalById(id) //
+		.orElseThrow(() -> new EntityNotFound(
+			String.format("Entity %1$s with id = '%2$s' is not found", id.getClass().getName(), id)));
+    }
+
+    Optional<T> optionalById(I id);
+
+    default T findByIdByPassCache(I id) throws EntityNotFound {
+	return optionalByIdByPassCache(id) //
+		.orElseThrow(() -> new EntityNotFound(
+			String.format("Entity %1$s with id = '%2$s' is not found", id.getClass().getName(), id)));
+    }
+
+    Optional<T> optionalByIdByPassCache(I id);
 
     <ET extends T> ET save(ET entity) throws PeristenceOperationFailed;
 
     <ET extends T> ET restore(ET entity) throws PeristenceOperationFailed, NotPersistedException;
 
-    void saveAll(List<T> entities) throws PeristenceOperationFailed;
-
+    default <ET extends T> Collection<ET> saveAll(Collection<ET> entities) throws PeristenceOperationFailed {
+	return MyObjects.requireNonNull(entities, "entities").stream() //
+		.map(this::save) //
+		.collect(Collectors.toList());
+    }
 }
