cbc02c687a69eebdc2a256bf08e9d32389e2ffa5
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-10-18 16:17:38 +0600

    new arquilian builder used

--- beans/src/test/java/test/dao/ArquillianBaseTestCase.java
+++ beans/src/test/java/test/dao/ArquillianBaseTestCase.java
@@ -1,35 +1,27 @@
 package test.dao;
 
-import static tech.lapsa.lapsa.arquillian.shrinkwrap.ShrinkWrapTools.*;
-
-import java.util.logging.Logger;
-
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
-import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.runner.RunWith;
 
 import tech.lapsa.insurance.dao.beans.AGeneralDAO;
+import tech.lapsa.lapsa.arquillian.archive.ArchiveBuilderFactory;
 
 @RunWith(Arquillian.class)
 public abstract class ArquillianBaseTestCase {
 
     @Deployment(testable = true)
     public static Archive<?> createDeploymentEAR() {
-	EnterpriseArchive ear = createEAR();
-	ear.addAsModule(createOwnEJB());
-	earAddRuntimeDependencies(ear);
-	Logger.getGlobal().info(ear.toString(true));
-	return ear;
-    }
-
-    private static JavaArchive createOwnEJB() {
-	JavaArchive ejb = createEJB();
-	ejb.addPackages(true, AGeneralDAO.class.getPackage());
-	jarAddManifestFolderRecursive(ejb);
-	Logger.getGlobal().info(ejb.toString(true));
-	return ejb;
+	return ArchiveBuilderFactory.newEarBuilder() //
+		.withModule(ArchiveBuilderFactory.newEjbBuilder() //
+			.withPackageOf(AGeneralDAO.class) //
+			.withManifestFolder() //
+			.build() //
+			.dumpingTo(System.out::println)) //
+		.withRuntimeDependencies()
+		.build() //
+		.dumpingTo(System.out::println) //
+		.asEnterpriseArchive();
     }
 }
