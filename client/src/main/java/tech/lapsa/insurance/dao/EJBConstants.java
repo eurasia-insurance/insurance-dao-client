dc81ac23d5b652ad60dd07496ddd2f688398b03d
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-12-16 10:24:37 +0600

    #cfrefact

--- client/src/main/java/tech/lapsa/insurance/dao/EJBConstants.java
+++ client/src/main/java/tech/lapsa/insurance/dao/EJBConstants.java
@@ -2,6 +2,6 @@ package tech.lapsa.insurance.dao;
 
 public interface EJBConstants {
 
-    public static final String APPLICATION_NAME = "insurance-dao-ear";
+    public static final String APPLICATION_NAME = "insurance-dao-app";
     public static final String MODULE_NAME = "insurance-dao";
 }
